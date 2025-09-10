1b8cae9682dada8853d5288cfbaf78620047e1aa
Erika Bibiana Rosero Reyes <20155033@tafe.wa.edu.au>
2025-09-10 10:59:30 +0800

    Added new cataloguing API

---
+++ catalogue.py
@@ -0,0 +1,22 @@
+"""
+This is a model. It does things realted to cataloguing
+"""
+
+def add_item (item, category):
+
+    """
+    This function lets you add an item to your collection of things.
+
+    Parameter
+    ------
+
+    item:the item to add
+    category: the category stuff into which tp insert things.
+
+    Return
+    -------
+    count : int
+
+    the number of items in teh collection, after adding the new one.
+
+    """
